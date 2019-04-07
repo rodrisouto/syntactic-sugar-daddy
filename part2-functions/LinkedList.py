c753553d88901d027e89de2bb233626fbc345bc5
Francisco Sicardi <francisco.sicardi@despegar.com>
2019-04-07 16:00:17 -0300

    linked list

---
+++ part2-functions/LinkedList.py
@@ -0,0 +1,29 @@
+
+class Node:
+    def __init__(self, n):
+        self.number = n
+        self.next = None
+
+
+class LinkedListIterator:
+    def __init__(self, linked_list):
+        self.curr = linked_list.head
+
+    def __next__(self):
+        n = self.curr
+        if n == None:
+            raise StopIteration
+        self.curr = self.curr.next
+        return n.number
+
+
+class LinkedList:
+    def __init__(self, numbers):
+        self.head = Node(numbers[0])
+        prev = self.head
+        for n in range(1, len(numbers)):
+            prev.next = Node(numbers[n])
+            prev = prev.next
+
+    def __iter__(self):
+        return LinkedListIterator(self)
