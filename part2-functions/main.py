026100986836d947d9e1a068bbf85cf0e3fe67b3
Francisco Sicardi <francisco.sicardi@despegar.com>
2019-04-10 09:43:32 -0300

    remove bunch of if statements

--- part2-functions/main.py
+++ part2-functions/main.py
@@ -26,18 +26,25 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage:\n\tpython3 main.py numbers.txt function", file=sys.stderr)
         exit(1)
+
     numbers = get_from_file(sys.argv[1])
     func_name = sys.argv[2]
 
-    results = []
-    if func_name == "maximo":
-        results = run_all(numbers, max_vector, max_list, max_ordered_vector, max_heap, populate_heap)
-    elif func_name == "media":
-        results = run_all(numbers, mean_vector, mean_list, mean_ordered_vector, mean_calculator, populate_mean_calculator)
-    else:
+    func_args = {
+        "maximo": (max_vector, max_list, max_ordered_vector, max_heap, populate_heap),
+        "media":  (mean_vector, mean_list, mean_ordered_vector, mean_calculator, populate_mean_calculator)
+    }
+
+    try:
+        args = func_args[func_name]
+    except KeyError:
         print("Function unknown: " + func_name, file=sys.stderr)
         exit(1)
 
+    results = run_all(numbers, *func_args[func_name])
     with open("resultado.txt", 'w') as file:
         file.write("\n".join([str(x) for x in results]))
     exit(0)
+
+
+
