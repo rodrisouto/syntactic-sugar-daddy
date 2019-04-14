0dec08e7900d7548253ebea033f30ba1cc4b82dd
milenamarchese <mile.marchese@hotmail.com>
2019-04-14 03:32:59 -0300

    gale_shapley w ties

--- gale-shapley/gale_shapley.py
+++ gale-shapley/gale_shapley.py
@@ -1,7 +1,4 @@
-from people import Person
-
-
-def gale_shapley_simple(men):
+def gale_shapley(men):
     pairs = set()
     while len(men):
         man = men.pop()
@@ -19,19 +16,4 @@ def gale_shapley_simple(men):
                 woman.engage(man)
                 man.engage(woman)
                 pairs.add((man, woman))
-    print(pairs)
-
-
-def gale_shapley():
-    w1 = Person("w1")
-    w2 = Person("w2")
-    m1 = Person("m1")
-    m2 = Person("m2")
-    w1.set_preferences([m1, m2])
-    w2.set_preferences([m2, m1])
-    m1.set_preferences([w1, w2])
-    m2.set_preferences([w2, w1])
-    gale_shapley_simple([m1, m2])
-
-
-gale_shapley()
+    return pairs
--- utils/people.py
+++ utils/people.py
@@ -1,36 +1,44 @@
 class Person:
 
     def __init__(self, name):
-        self.partner = None
+        self.partners = []
         self.preferences = None
         self.name = name
+        self.positions = {}
 
     def __repr__(self):
         return self.name
 
     def set_preferences(self, people):
         self.preferences = people
+        for i in range(len(people)):
+            for j in range(len(people[i])):
+                self.positions[people[i][j]] = (i, j)
 
     def engage(self, person):
-        self.partner = person
+        self.partners.append(person)
 
     def get_preferred(self):
         if len(self.preferences):
-            return self.preferences[0]
+            return self.preferences.pop(0)
 
     def is_free(self):
-        return not self.partner
+        return not len(self.partners)
 
-    def prefers(self, person):
-        index1 = self.preferences.index(person)
-        index2 = self.preferences.index(self.partner)
-        value = True if (index1 < index2) else False
-        return value
+    def get_rejected(self, person):
+        preference = self.positions[person][0]
+        rejected = []
+        for i in self.partners:
+            if self.positions[i][0] < preference:
+                rejected.append(i)
+                self.partners.remove(i)
+        return rejected
 
-    def release(self):
-        ex = self.partner
-        self.partner = None
-        return ex
+    def is_multiply_engaged(self):
+        return len(self.partners) > 1
 
-    def free(self):
-        self.partner = None
+    def get_partners(self):
+        return self.partners
+
+    def free(self, partner):
+        self.partners.remove(partner)
