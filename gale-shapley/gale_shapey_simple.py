24c690c0f7b754e2e73110488cb35ecb37568ce2
milenamarchese <mile.marchese@hotmail.com>
2019-04-14 00:18:44 -0300

    almost working gale-shapley

--- gale-shapley/gale_shapey_simple.py
+++ gale-shapley/gale_shapey_simple.py
@@ -1,18 +1,37 @@
+from people import Person
 
-def gale_shapley_simple(men, women):
-#Initially all m ∈ M and w ∈ W are free
-    free_men = men
-    while not free_men.empty():
-        man = free_men.pop()
-        woman = man.get_prefered()
-        if woman.free():
+
+def gale_shapley_simple(men):
+    pairs = set()
+    while len(men):
+        man = men.pop()
+        woman = man.get_preferred()
+        if woman.is_free():
             woman.engage(man)
             man.engage(woman)
-        if woman.engaged():
-            if not woman.prefers(man):
-                #m is free
-            else:
-                woman.release().free()
+            pairs.add((man, woman))
+        if not woman.is_free():
+            if woman.prefers(man):
+                pairs.remove((woman.get_partner(), man))
+                ex = woman.release()
+                ex.free()
+                men.append(ex)
                 woman.engage(man)
                 man.engage(woman)
-        return S
+                pairs.add((man, woman))
+    print(pairs)
+
+
+def gale_shapley():
+    w1 = Person("w1")
+    w2 = Person("w2")
+    m1 = Person("m1")
+    m2 = Person("m2")
+    w1.set_preferences([m1, m2])
+    w2.set_preferences([m2, m1])
+    m1.set_preferences([w1, w2])
+    m2.set_preferences([w2, w1])
+    gale_shapley_simple([m1, m2])
+
+
+gale_shapley()
--- utils/people.py
+++ utils/people.py
@@ -1,30 +1,36 @@
-class Man:
-    def __init__(self, women):
+class Person:
+
+    def __init__(self, name):
         self.partner = None
-        self.preferences = women
+        self.preferences = None
+        self.name = name
 
-    def engage(self, woman):
-        self.partner = woman
+    def __repr__(self):
+        return self.name
 
-    def get_prefered(self):
-        return self.preferences.pop()
+    def set_preferences(self, people):
+        self.preferences = people
 
-    def free(self):
-        self.partner = None
+    def engage(self, person):
+        self.partner = person
 
+    def get_preferred(self):
+        if len(self.preferences):
+            return self.preferences[0]
 
-class Woman:
-    def __init__(self, men):
-        self.partner = None
-        self.preferences = men
+    def is_free(self):
+        return not self.partner
 
-    def engage(self, man):
-        self.partner = man
-
-    def prefers(self, man):
-        #compare
+    def prefers(self, person):
+        index1 = self.preferences.index(person)
+        index2 = self.preferences.index(self.partner)
+        value = True if (index1 < index2) else False
+        return value
 
     def release(self):
         ex = self.partner
         self.partner = None
         return ex
+
+    def free(self):
+        self.partner = None
