c8fbf0d1f9826338202a67b84a77eb8034433a97
milenamarchese <mmarchese@fi.uba.ar>
2019-04-25 15:24:07 -0300

    fixed standard deviation

--- tp1/collection-functions/standard_deviation.py
+++ tp1/collection-functions/standard_deviation.py
@@ -7,7 +7,7 @@ def standard_deviation_vector(vector):
     deviation = 0
     for i in range(len(vector)):
         deviation += (vector[i] - mean) ** 2
-    return (deviation / len(vector)) ** 1/2
+    return (deviation / len(vector)) ** (1/2)
 
 
 def standard_deviation_list(linked_list):
@@ -15,7 +15,7 @@ def standard_deviation_list(linked_list):
     deviation = 0
     for n in linked_list:
         deviation += (n - mean) ** 2
-    return (deviation / len(linked_list)) ** 1/2
+    return (deviation / len(linked_list)) ** (1/2)
 
 
 def standard_deviation_ordered_vector(ordered_vector):
@@ -24,4 +24,3 @@ def standard_deviation_ordered_vector(ordered_vector):
 
 def standard_deviation_STD_calculator(std_calculator):
     return std_calculator.getStdDeviation()
-
