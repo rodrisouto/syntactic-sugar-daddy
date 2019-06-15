ce91d68f7bea9cc13e11d8f8b6cffc7c8d5c242a
Francisco Sicardi <francisco.sicardi@despegar.com>
2019-06-15 16:45:35 -0300

    correction

--- tp2/suspects/suspects.py
+++ tp2/suspects/suspects.py
@@ -14,6 +14,9 @@ class Person:
     def minutes_shared_with(self, other_person):
         return self.exit - other_person.entry
 
+    def exits_at_same_time(self, other_person):
+        return self.exit == other_person.exit
+
     def __str__(self):
         return self.name
 
@@ -56,16 +59,21 @@ def find_suspects(people):
     for person in people:
         next_to_leave = building.top()
         while next_to_leave is not None and not next_to_leave.shares_time_with(person):
+            same_time = [building.pop()]
+            while building.top() is not None and next_to_leave.exits_at_same_time(building.top()):
+                same_time.append(building.pop())
             append_if_are_suspects(suspects, people_inside, next_to_leave)
-            building.pop()
-            people_inside.remove(next_to_leave)
+            for next in same_time:
+                people_inside.remove(next)
             next_to_leave = building.top()
         people_inside.insert(person)
         building.push(person)
 
     while len(people_inside) > 4:
-        next_to_leave = building.top()
-        append_if_are_suspects(suspects, people_inside, next_to_leave)
-        people_inside.remove(next_to_leave)
-        building.pop()
+        next_to_leave = [building.pop()]
+        while building.top() is not None and next_to_leave[0].exits_at_same_time(building.top()):
+            next_to_leave.append(building.pop())
+        append_if_are_suspects(suspects, people_inside, next_to_leave[0])
+        for next in next_to_leave:
+            people_inside.remove(next)
     return suspects
